b1d7afda2f88103c74d4fc494fabec4fa1c36211
Joris Leker <joris.leker@valsplat.nl>
2019-12-23 15:26:58 +0100

    #2 HoursType missed some changes

--- src/Valsplat/Simplicate/Entities/HoursType.php
+++ src/Valsplat/Simplicate/Entities/HoursType.php
@@ -5,44 +5,20 @@ namespace Valsplat\Simplicate\Entities;
 use Valsplat\Simplicate\Actions;
 use Valsplat\Simplicate\Entity;
 
-class ProjectService extends Entity
+class HoursType extends Entity
 {
     use Actions\Getable;
     use Actions\Listable;
     use Actions\Storable;
-    use Actions\Deletable;
+    //use Actions\Deletable;
 
     protected $fillable = [
-        'project_id',
-        'service_number',
-        'related_service_id',
-        'expected_invoice_date',
-        'invoice_date',
-        'status',
-        'hour_types',
-        'cost_types',
-        'vat_class',
-        'revenue_group',
-        'invoice_in_installments',
-        'installments',
-        'created_at',
-        'updated_at',
-        'budget_financial_hours',
-        'write_hours_start_date',
-        'write_hours_end_date',
-        'start_date',
-        'end_date',
-        'subscription_cycle',
-        'budget',
-        'default_service_id',
-        'name',
-        'invoice_method', // ['Hours','Subscription','Fixed']
-        'amount',
-        'price',
-        'track_hours',
-        'track_cost'
+        'label',
+        'tariff',
+        'blocked',
+        'color'
     ];
 
-    protected $endpoint = 'projects/service/';
+    protected $endpoint = 'hours/hourstype/';
     // protected $namespace = '';
 }
